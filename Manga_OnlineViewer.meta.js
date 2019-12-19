b8a6cf7bae7b435cd939fb26b2ce149de2d8a204
TagoDR <TagoDR@users.noreply.github.com>
2019-12-19 15:03:07 +0000

    Regenerate build artifacts.

--- Manga_OnlineViewer.meta.js
+++ Manga_OnlineViewer.meta.js
@@ -7,7 +7,7 @@
 // @description Shows all pages at once in online view for these sites: Batoto, ComiCastle, ReadComicsOnline, Dynasty-Scans, EatManga, Easy Going Scans, FoOlSlide, KissManga, MangaDoom, MangaFox, MangaGo, MangaHere, MangaInn, MangaLyght, MangaPark, MangaReader,MangaPanda, MangaStream, MangaTown, NineManga, ReadManga Today, SenManga(Raw), TenManga, TheSpectrum, MangaDeep, Funmanga, UnionMangas, MangaHost, Hoc Vien Truyen Tranh, JaiminisBox, MangaDex, HatigarmScans, MangaRock, MangaKakalot,MangaNelo, LHTranslation, JapScan.To, MangaSee, MangaZuki
 // @version 15.03.0
 // @license MIT
-// @date 2019-11-24
+// @date 2019-12-19
 // @grant GM_getValue
 // @grant GM_setValue
 // @grant GM_listValues
