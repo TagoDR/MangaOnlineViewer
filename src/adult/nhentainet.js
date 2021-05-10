3a9c5de10a1ede2cc517cf4ab2eb08d9c3aa1143
TagoDR <t@t.com>
2021-05-10 14:24:13 -0300

    Fixed nhentainet

--- src/adult/nhentainet.js
+++ src/adult/nhentainet.js
@@ -7,14 +7,15 @@ export default {
   category: 'hentai',
   run() {
     const num = parseInt($('.num-pages:first').html(), 10);
-    const src = $('#image-container img').attr('src').replace(/\d+.jpg/, '');
+    const ext = $('#image-container img').attr('src').match(/\.\w+$/);
+    const src = $('#image-container img').attr('src').replace(ext, '');
     return {
       title: $('title').text().split('- Page')[0].trim(),
       series: $('.go-back').attr('href'),
       quant: num,
       prev: '#',
       next: '#',
-      listImages: [...Array(num).keys()].map((i) => `${src}/${i + 1}.jpg`),
+      listImages: [...Array(num).keys()].map((i) => `${src}/${i + 1}${ext}`),
     };
   },
 };
