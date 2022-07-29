8a9ab3d8d1d52761046293978f79c13c7f9d0d64
TagoDR <TagoDR@github.com>
2022-07-29 08:26:49 -0300

    Fix Mangapark

--- src/main/mangapark.ts
+++ src/main/mangapark.ts
@@ -1,14 +1,8 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 // == MangaPark ====================================================================================
 declare let CryptoJS: any;
 declare let amWord: string;
 declare let amPass: string;
-declare let amSub_name: string;
-declare let mpEpi_name: string;
-declare let currSubUrl: string;
 declare let imgPathLis: string[];
-declare let prevEpiUrl: string;
-declare let nextEpiUrl: string;
 declare let imgCdnHost: string;
 export default {
   name: 'MangaPark',
@@ -20,11 +14,11 @@ export default {
   run() {
     const pass = JSON.parse(CryptoJS.AES.decrypt(amWord, amPass).toString(CryptoJS.enc.Utf8));
     return {
-      title: `${amSub_name} - ${mpEpi_name}`,
-      series: currSubUrl,
+      title: document.querySelector('title')?.textContent?.trim(),
+      series: document.querySelector('.nav-title a')?.getAttribute('href'),
       pages: imgPathLis.length,
-      prev: prevEpiUrl,
-      next: nextEpiUrl,
+      prev: document.querySelector('.nav-prev a')?.getAttribute('href'),
+      next: document.querySelector('.nav-next a')?.getAttribute('href'),
       listImages: imgPathLis.map((i: string, index: number) => `${imgCdnHost + i}?${pass[index]}`),
     };
   },
