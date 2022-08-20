7b3297544338386a4afd3a6abdc29d3303d0a5d6
TagoDR <TagoDR@github.com>
2022-08-19 23:03:18 -0300

    Fix Settings/Bookmarks

--- src/utils/diffObj.ts
+++ src/utils/diffObj.ts
@@ -14,10 +14,10 @@ import _ from 'lodash';
  *  But this works, is easier to understand that the "correct"
  *  implementation, and entertained me for a bit. *
  *
- *  http://stackoverflow.com/questions/31295545/how-to-get-only-the-changed-values-from-two-json-objects
- *  https://gist.github.com/Yimiprod/7ee176597fef230d1451
- *  https://stackoverflow.com/questions/8572826/generic-deep-diff-between-two-objects
- *  https://qastack.com.br/programming/31683075/how-to-do-a-deep-comparison-between-2-objects-with-lodash
+ * http://stackoverflow.com/questions/31295545/how-to-get-only-the-changed-values-from-two-json-objects
+ * https://gist.github.com/Yimiprod/7ee176597fef230d1451
+ * https://stackoverflow.com/questions/8572826/generic-deep-diff-between-two-objects
+ * https://qastack.com.br/programming/31683075/how-to-do-a-deep-comparison-between-2-objects-with-lodash
  * @param  {Object} changed Object compared
  * @param  {Object} original   Object to compare with
  * @return {Object} Return a new object who represent the diff
@@ -28,7 +28,9 @@ const diffObj = <T extends object>(changed: T, original: T): Partial<object> => 
       object,
       (result: any, value: any, key: keyof object) => {
         if (!_.isEqual(value, base[key])) {
-          if (_.isObject(value)) {
+          if (_.isArray(value)) {
+            result[key] = _.difference(value, base[key]);
+          } else if (_.isObject(value) && _.isObject(base[key])) {
             result[key] = changes(value, base[key]);
           } else {
             result[key] = value;
@@ -39,4 +41,31 @@ const diffObj = <T extends object>(changed: T, original: T): Partial<object> => 
     );
   return changes(changed, original);
 };
+
+// const getDiff = (changed: any, original: any) => {
+//   const accumulator = _.isArray(original) ? [] : {};
+//   function recursiveDiff(base: any, object: any, result: any) {
+//     for (const key in base) {
+//       const value: object = object[key];
+//
+//       if (!_.isEqual(base[key], value)) {
+//         if (_.isArray(value)) {
+//           // if array
+//           result[key] = [];
+//           recursiveDiff(base[key], value, result[key]);
+//         } else if (_.isObject(value)) {
+//           // if object
+//           result[key] = {};
+//           recursiveDiff(base[key], value, result[key]);
+//         } else {
+//           // if value
+//           result[key] = value;
+//         }
+//       }
+//     }
+//   }
+//   recursiveDiff(original, changed, accumulator);
+//   return accumulator;
+// };
+
 export default diffObj;
