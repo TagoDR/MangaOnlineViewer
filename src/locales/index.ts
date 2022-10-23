f5d0c5fd75e576c172de3186b4f2cc1cc671aff9
TagoDR <TagoDR@github.com>
2022-10-23 16:33:00 -0300

    Fix Locales Import

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,5 +1,5 @@
-import en_US from './en_US.json';
-import pt_BR from './pt_BR.json';
+import en_US from './en_US.json' assert { type: 'json' };
+import pt_BR from './pt_BR.json' assert { type: 'json' };
 import { ILocale } from '../types/ILocale';
 
 const locales = [en_US, pt_BR] as ILocale[];
