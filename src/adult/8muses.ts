572bde7f72ee32fe828ceb87dc5c2a9cc25962c9
TagoDR <TagoDR@github.com>
2023-06-27 10:16:11 -0300

    Fix 8muses

--- src/adult/8muses.ts
+++ src/adult/8muses.ts
@@ -1,45 +1,22 @@
 // == 8Muses =======================================================================================
 export default {
   name: '8Muses',
-  url: /https?:\/\/comics.8muses.com\/comics\/picture\/.+/,
+  url: /https?:\/\/8muses.io\/picture\/.+/,
   homepage: 'https://comics.8muses.com/',
   language: ['English'],
   category: 'hentai',
   async run() {
-    function decode(data: string) {
-      /* eslint-disable no-mixed-operators,no-shadow */
-      return ((t) => {
-        if (!t.startsWith('!')) return t;
-        return t
-          .slice(1)
-          .replace(/[\x21-\x7e]/g, (s) => String.fromCharCode(33 + ((s.charCodeAt(0) + 14) % 94)));
-      })(data.replace(/&gt;/g, '>').replace(/&lt;/g, '<').replace(/&amp;/g, '&'));
-      /* eslint-enable no-mixed-operators,no-shadow */
-    }
-
-    const api = await fetch(window.location.href)
-      .then((res) => res.text())
-      .then((html) => new DOMParser().parseFromString(html, 'text/html'));
-    const dataPublic = JSON.parse(
-      decode(api.querySelector('#ractive-public')?.innerHTML.trim() ?? ''),
-    );
-    const dataShared = JSON.parse(
-      decode(api.querySelector('#ractive-shared')?.innerHTML.trim() ?? ''),
-    );
-    const src = dataShared.options.pictureHost ?? window.location.host;
+    const W: any = typeof unsafeWindow !== 'undefined' ? unsafeWindow : window;
+    W.link_images.shift();
     return {
       title: document
         .querySelector('.top-menu-breadcrumb li:nth-last-child(2) a')
         ?.textContent?.trim(),
-      series: document
-        .querySelector('.top-menu-breadcrumb li:nth-last-child(2) a')
-        ?.getAttribute('href'),
-      pages: dataPublic.pictures.length,
+      series: W.post_id,
+      pages: W.link_images.length,
       prev: '#',
       next: '#',
-      listImages: dataPublic.pictures.map(
-        (img: { publicUri: string }) => `//${src}/image/fl/${img.publicUri}.jpg`,
-      ),
+      listImages: W.link_images,
     };
   },
 };
