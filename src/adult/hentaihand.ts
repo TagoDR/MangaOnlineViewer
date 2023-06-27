79f14bde11d3e6da8168b520a37e7ca640751f51
TagoDR <TagoDR@github.com>
2023-06-27 10:54:25 -0300

    Fixed hentaihand

--- src/adult/hentaihand.ts
+++ src/adult/hentaihand.ts
@@ -1,20 +1,20 @@
 // == HentaiHand ==================================================================================
 export default {
   name: ['HentaiHand', 'nHentai.com'],
-  url: /https?:\/\/(www.)?(hentaihand|nhentai).com\/.+\/reader\/\d+/,
+  url: /https?:\/\/(www.)?(hentaihand|nhentai).com\/.+\/reader/,
   homepage: ['https://hentaihand.com/', 'https://nhentai.com'],
   language: ['English'],
   category: 'hentai',
-  waitEle: '.vertical-image',
+  waitEle: '.reader img',
   run() {
-    const images = [...document.querySelectorAll('.vertical-image img')];
+    const images = [...document.querySelectorAll('.reader img')];
     return {
       title: document.querySelector('.reader-header h5')?.textContent?.trim(),
       series: document.querySelector('.reader-header h5 a')?.getAttribute('href'),
       pages: images.length,
       prev: '#',
       next: '#',
-      listImages: images.map((img) => img.getAttribute('data-src')),
+      listImages: images.map((img) => img.getAttribute('data-src') ?? img.getAttribute('src')),
     };
   },
 };
