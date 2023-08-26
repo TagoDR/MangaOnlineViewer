9eee34306c1ba6379d7f0d43429a7e62f2a730f4
TagoDR <TagoDR@github.com>
2023-08-26 09:43:36 -0300

    Updated 8Muses

--- src/adult/8muses.ts
+++ src/adult/8muses.ts
@@ -1,22 +1,60 @@
 // == 8Muses =======================================================================================
+import { waitForAtb } from '../utils/waitFor';
+
 export default {
-  name: '8Muses',
-  url: /https?:\/\/8muses.io\/picture\/.+/,
-  homepage: 'https://comics.8muses.com/',
+  name: ['8Muses.com', '8Muses.io'],
+  obs: 'Slow start, bruteforce may be required',
+  url: /https?:\/\/(comics.)?8muses.(com|io)\/(comics\/)?picture\/.+/,
+  homepage: ['https://comics.8muses.com/', 'https://8muses.io/'],
   language: ['English'],
   category: 'hentai',
   async run() {
     const W: any = typeof unsafeWindow !== 'undefined' ? unsafeWindow : window;
-    W.link_images.shift();
+    const img = W.link_images?.slice(1, W.link_images.length);
+    const num =
+      img?.length ??
+      parseInt(
+        document.querySelector('link[rel="last"]')?.getAttribute('href')?.match(/\d+$/)?.at(0) ??
+          '',
+        10,
+      );
     return {
-      title: document
+      title: [...document.querySelectorAll('.top-menu-breadcrumb li:not(:last-child)')]
+        .map((e) => e?.textContent?.trim())
+        .join('/'),
+      series: document
         .querySelector('.top-menu-breadcrumb li:nth-last-child(2) a')
-        ?.textContent?.trim(),
-      series: W.post_id,
-      pages: W.link_images.length,
+        ?.getAttribute('href'),
+      pages: num,
       prev: '#',
       next: '#',
-      listImages: W.link_images,
+      listImages: img,
+      async before() {
+        if (!W.link_images?.length) {
+          const div = document.createElement('div');
+          div.setAttribute(
+            'style',
+            'height: 100vh;width: 100vw;position: fixed;top: 0;left: 0;z-index: 100000;background: white;opacity: 0.5;',
+          );
+          document.body.append(div);
+          const prev = document.querySelector('.page-prev');
+          while (
+            document.querySelector('.c-dropdown-toggle')?.textContent?.match(/\d+/)?.at(0) !== '1'
+          ) {
+            prev?.dispatchEvent(new Event('click'));
+          }
+          const next = document.querySelector('.page-next');
+          const target = document.querySelector<HTMLDivElement>('.p-picture');
+          const src = [];
+          for (let i = 1; i <= this.pages; i += 1) {
+            // eslint-disable-next-line no-await-in-loop
+            src[i - 1] = await waitForAtb('.photo img', 'src', target ?? document.body);
+            target?.querySelector('img')?.removeAttribute('src');
+            next?.dispatchEvent(new Event('click'));
+          }
+          this.listImages = src;
+        }
+      },
     };
   },
 };
