0cd7bcc83763a68f2c06a88c4691673327ecb401
TagoDR <TagoDR@users.noreply.github.com>
2023-10-01 22:53:52 +0000

    Apply changes to release

--- dist/Manga_OnlineViewer.user.js
+++ dist/Manga_OnlineViewer.user.js
@@ -92,26 +92,25 @@
         if (window.location.search !== "?load=2") {
           window.location.search = "?load=2";
         }
-        const images = [...document.querySelectorAll('div[name="image-item"] img')];
+        const images2 = [...document.querySelectorAll('div[name="image-item"] img')];
         return {
           title: document.querySelector("h6")?.textContent?.trim(),
           series: document.querySelector("h3 a")?.getAttribute("href"),
-          pages: images.length,
+          pages: images2.length,
           prev: [...document.querySelectorAll("span")].find((item) => item.textContent?.endsWith("Prev Chapter"))?.parentElement?.getAttribute("href"),
           next: [...document.querySelectorAll("span")].find((item) => item.textContent?.startsWith("Next Chapter"))?.parentElement?.getAttribute("href"),
-          listImages: images.map((img) => img.getAttribute("src"))
-        };
-      } else {
-        const images = [...document.querySelectorAll(".page-img")];
-        return {
-          title: document.querySelector(".nav-title a")?.textContent?.trim(),
-          series: document.querySelector(".nav-title a")?.getAttribute("href"),
-          pages: images.length,
-          prev: document.querySelector(".nav-prev a")?.getAttribute("href"),
-          next: document.querySelector(".nav-next a")?.getAttribute("href"),
-          listImages: images.map((img) => img.getAttribute("src"))
+          listImages: images2.map((img) => img.getAttribute("src"))
         };
       }
+      const images = [...document.querySelectorAll(".page-img")];
+      return {
+        title: document.querySelector(".nav-title a")?.textContent?.trim(),
+        series: document.querySelector(".nav-title a")?.getAttribute("href"),
+        pages: images.length,
+        prev: document.querySelector(".nav-prev a")?.getAttribute("href"),
+        next: document.querySelector(".nav-next a")?.getAttribute("href"),
+        listImages: images.map((img) => img.getAttribute("src"))
+      };
     }
   };
 
