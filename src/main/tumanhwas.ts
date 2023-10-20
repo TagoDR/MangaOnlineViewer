b728a602e86ca7447a8f30a70912ad609f9de4ce
TagoDR <TagoDR@github.com>
2023-10-20 17:58:11 -0300

    Fixed TuManhwas

--- src/main/tumanhwas.ts
+++ src/main/tumanhwas.ts
@@ -1,19 +1,18 @@
 // == TuManhwas  ===================================================================================
 export default {
   name: 'TuManhwas',
-  url: /https?:\/\/(www.)?tumanhwas.com\/view\/.+/,
+  url: /https?:\/\/(www.)?tumanhwas.com\/news\/.+/,
   homepage: 'https://tumanhwas.com/',
   language: ['Spanish'],
   category: 'manga',
   run() {
-    const chapter = Array.from(document.querySelectorAll('.listupd a'));
     const images = [...document.querySelectorAll('#chapter_imgs img')];
     return {
-      title: document.querySelector('.releases h1')?.textContent?.trim(),
-      series: chapter.find((el) => el.textContent?.includes('Lista'))?.getAttribute('href'),
+      title: document.querySelector('.entry-title')?.textContent?.trim(),
+      series: document.querySelector('.nextprev a:nth-child(2)')?.getAttribute('href'),
       pages: images.length,
-      prev: chapter.find((el) => el.textContent?.includes('Anterior'))?.getAttribute('href'),
-      next: chapter.find((el) => el.textContent?.includes('Siguiente'))?.getAttribute('href'),
+      prev: document.querySelector('.nextprev a:nth-child(1)')?.getAttribute('href'),
+      next: document.querySelector('.nextprev a:nth-child(3)')?.getAttribute('href'),
       listImages: images.map((item) => $(item).attr('src')),
     };
   },
