609f20b13fcbc76482e51f0355b8b8bce15cd228
TagoDR <TagoDR@github.com>
2023-12-16 11:01:37 -0300

    Placeholder rulers

--- src/utils/svgs.ts
+++ src/utils/svgs.ts
@@ -14,8 +14,8 @@ function svgToUrl(str: string) {
   return `data:image/svg+xml;charset=UTF-8,${encoded}`;
 }
 
-function placeholder(width: number, height: number, bgColor = '#0F1C3F', textColor = '#ECEAD9') {
-  const str = html`
+function rectangle(width: number, height: number, bgColor: string, textColor: string) {
+  return html`
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="${width}"
@@ -37,6 +37,103 @@ function placeholder(width: number, height: number, bgColor = '#0F1C3F', textCol
       </text>
     </svg>
   `;
+}
+
+const rulerMarkerLength = (len: number) => {
+  if (len % 100 === 0) {
+    return 15;
+  }
+  if (len % 50 === 0) {
+    return 10;
+  }
+  if (len % 25 === 0) {
+    return 5;
+  }
+  return 2.5;
+};
+
+// A function that generates a svg rectangle code with given width and height
+// Inside the svg is a ruler to measure pixels horizontally and vertically
+function rectangleRuler(width: number, height: number, bgColor: string, textColor: string) {
+  let markers = '';
+
+  // Create a loop to draw the horizontal ruler ticks and labels
+  for (let x = 0; x <= width; x += 5) {
+    // Create a line element for each tick using string templates
+    const tick = html` <line x1="${x}" y1="0" x2="${x}" y2="${rulerMarkerLength(x)}" />`;
+    markers += tick;
+
+    // Create a text element for each label using string templates
+    if (x !== 0 && x % 50 === 0) {
+      const label = html` <text
+        x="${x}"
+        y="25"
+        text-anchor="middle"
+        font-size="${rulerMarkerLength(x)}px"
+      >
+        ${x}
+      </text>`;
+      markers += label;
+    }
+  }
+
+  // Create a loop to draw the vertical ruler ticks and labels
+  for (let y = 0; y <= height; y += 5) {
+    // Create a line element for each tick using string templates
+    const tick = html` <line x1="0" y1="${y}" x2="${rulerMarkerLength(y)}" y2="${y}" />`;
+    markers += tick;
+
+    // Create a text element for each label using string templates
+    if (y !== 0 && y % 50 === 0) {
+      const label = html` <text
+        x="25"
+        y="${y}"
+        text-anchor="middle"
+        dominant-baseline="middle"
+        font-size="${rulerMarkerLength(y)}px"
+      >
+        ${y}
+      </text>`;
+      markers += label;
+    }
+  }
+
+  // Create a svg element with the given width and height using string templates
+  // Return the svg element as a string
+  return html` <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="${width}"
+    height="${height}"
+    viewBox="0 0 ${width} ${height}"
+  >
+    <rect width="${width}" height="${height}" fill="${bgColor}" />
+    <text
+      fill="${textColor}"
+      font-family="Verdana, Arial, Helvetica, sans-serif"
+      font-size="30"
+      dy="10.5"
+      font-weight="bold"
+      x="50%"
+      y="50%"
+      text-anchor="middle"
+    >
+      ${width}x${height}
+    </text>
+    <g
+      stroke-width="1"
+      font-family="Verdana, Arial, Helvetica, sans-serif"
+      font-size="10px"
+      font-weight="100"
+      fill="${textColor}"
+      stroke="${textColor}"
+    >
+      ${markers}
+    </g>
+  </svg>`;
+}
+
+function placeholder(width: number, height: number, bgColor = '#0F1C3F', textColor = '#ECEAD9') {
+  const str = rectangleRuler(width, height, bgColor, textColor);
   return svgToUrl(str);
 }
 
@@ -51,4 +148,4 @@ function randomPlaceholder() {
   return placeholder(widths[randomWidth], heights[randomHeight], backgrounds[randomColor]);
 }
 
-export { randomPlaceholder, placeholder, svgToUrl };
+export { randomPlaceholder, placeholder, svgToUrl, rectangle, rectangleRuler };
