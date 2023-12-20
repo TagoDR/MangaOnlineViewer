71f6ec3e6c10f5d720ac719c47f41e6eecd2a337
TagoDR <TagoDR@github.com>
2023-12-20 14:26:13 -0300

    Fix OmegaScans

--- src/adult/omegascans.ts
+++ src/adult/omegascans.ts
@@ -1,24 +1,10 @@
+import yugenmangas from '../main/yugenmangas';
 // == OmegaScans ===================================================================================
-/* eslint-disable no-underscore-dangle */
 export default {
+  ...yugenmangas,
   name: ['OmegaScans'],
   url: /https?:\/\/(www\.)?(omegascans).(org)\/.+/,
   homepage: ['https://omegascans.org/'],
   language: ['English'],
-  category: 'manga',
-  waitVar: '__NEXT_DATA__',
-  async run() {
-    const api = await fetch(
-      `https://api.omegascans.org/series/chapter/${unsafeWindow.__NEXT_DATA__.props.pageProps.data.id}`,
-    ).then(async (res) => res.json());
-    const { images } = api.content;
-    return {
-      title: document.querySelector('h5')?.textContent?.trim(),
-      series: document.querySelector('h5 a')?.getAttribute('href'),
-      pages: images.length,
-      prev: document.querySelector('.fa-arrow-left')?.closest('a')?.getAttribute('href'),
-      next: document.querySelector('.fa-arrow-right')?.closest('a')?.getAttribute('href'),
-      listImages: images,
-    };
-  },
+  category: 'hentai',
 };
