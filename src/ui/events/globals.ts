8570bec386d427314d848e5273466128ea61f3ed
TagoDR <TagoDR@github.com>
2024-02-12 16:18:01 -0300

    Fix Series link

--- src/ui/events/globals.ts
+++ src/ui/events/globals.ts
@@ -17,19 +17,16 @@ export function buttonGlobalHideImageControls() {
   document.querySelector('#MangaOnlineViewer')?.classList.toggle('hideControls');
 }
 
-function buttonRedirectURL(back: boolean = false) {
-  return (event: Event) => {
-    const element = event.target as HTMLElement;
-    const url = element.getAttribute('value') ?? element.getAttribute('href');
-    if ((event as MouseEvent).button !== 1 && !(event as MouseEvent).ctrlKey) {
-      event.preventDefault();
-      if (url) {
-        window.location.href = url;
-      } else if (back) {
-        window.history.back();
-      }
+function buttonRedirectURL(event: Event) {
+  const element = event.target as HTMLElement;
+  const url = element.getAttribute('value') ?? element.getAttribute('href');
+  if ((event as MouseEvent).button !== 1 && !(event as MouseEvent).ctrlKey) {
+    if (url) {
+      window.location.href = url;
+    } else if (element.id === 'series') {
+      window.history.back();
     }
-  };
+  }
 }
 
 export function buttonCommentsOpen() {
@@ -51,9 +48,9 @@ export function changeCommentsColor() {
 function globals() {
   document.querySelector('#download')?.addEventListener('click', buttonStartDownload);
   document.querySelector('#pageControls')?.addEventListener('click', buttonGlobalHideImageControls);
-  document.querySelector('#next')?.addEventListener('click', buttonRedirectURL());
-  document.querySelector('#prev')?.addEventListener('click', buttonRedirectURL());
-  document.querySelector('#series')?.addEventListener('click', buttonRedirectURL(true));
+  document.querySelector('#next')?.addEventListener('click', buttonRedirectURL);
+  document.querySelector('#prev')?.addEventListener('click', buttonRedirectURL);
+  document.querySelector('#series')?.addEventListener('click', buttonRedirectURL);
   document.querySelector('#CommentsButton')?.addEventListener('click', buttonCommentsOpen);
   document.querySelector('#CommentsColorScheme')?.addEventListener('click', changeCommentsColor);
   document.querySelectorAll('.closeButton')?.forEach(addEvent('click', buttonCommentsClose));
