02512a449f518177453fc4c01faa4d75bf922d69
TagoDR <TagoDR@github.com>
2024-02-13 10:58:02 -0300

    Fix MangaDex

--- src/main/mangadex.ts
+++ src/main/mangadex.ts
@@ -5,13 +5,13 @@ export default {
   homepage: 'https://mangadex.org/',
   language: ['English'],
   category: 'manga',
-  waitEle: '.md--reader-menu a',
+  waitEle: '#chapter-selector a',
   async run() {
     const chapterId = /\/chapter\/([^/]+)(\/\d+)?/.exec(window.location.pathname)?.at(1);
     const home = `https://api.mangadex.org/at-home/server/${chapterId}`;
     const server = await fetch(home).then(async (res) => res.json());
     const images = server.chapter.data;
-    const chapters = document.querySelectorAll(".md--reader-menu a[href^='/chapter/']");
+    const chapters = document.querySelectorAll('#chapter-selector a');
     return {
       title: document.querySelector('title')?.text.replace(' - MangaDex', ''),
       series: document.querySelector("a.text-primary[href^='/title/']")?.getAttribute('href'),
