03b57ba69aa2363c7951906de3bba24cf27a1ab8
TagoDR <TagoDR@github.com>
2024-02-13 10:27:13 -0300

    Fix MangaDex

--- src/main/mangadex.ts
+++ src/main/mangadex.ts
@@ -5,21 +5,19 @@ export default {
   homepage: 'https://mangadex.org/',
   language: ['English'],
   category: 'manga',
-  waitEle: ".md--reader-menu a[href^='/chapter/']",
+  waitEle: '.md--reader-menu a',
   async run() {
     const chapterId = /\/chapter\/([^/]+)(\/\d+)?/.exec(window.location.pathname)?.at(1);
     const home = `https://api.mangadex.org/at-home/server/${chapterId}`;
     const server = await fetch(home).then(async (res) => res.json());
     const images = server.chapter.data;
-    const chapters = [...document.querySelectorAll(".md--reader-menu a[href^='/chapter/']")].map(
-      (a) => a.getAttribute('href'),
-    );
+    const chapters = document.querySelectorAll(".md--reader-menu a[href^='/chapter/']");
     return {
       title: document.querySelector('title')?.text.replace(' - MangaDex', ''),
       series: document.querySelector("a.text-primary[href^='/title/']")?.getAttribute('href'),
       pages: images.length,
-      prev: chapters[0] !== window.location.pathname ? chapters[0] : '#',
-      next: chapters[1] !== window.location.pathname ? chapters[1] : '#',
+      prev: chapters?.item(0)?.getAttribute('href'),
+      next: chapters?.item(1)?.getAttribute('href'),
       listImages: images.map(
         (img: string) => `${server.baseUrl}/data/${server.chapter.hash}/${img}`,
       ),
