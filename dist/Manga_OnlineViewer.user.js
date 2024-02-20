adac534205d8a171e946f4c98959a318e274ae05
TagoDR <TagoDR@users.noreply.github.com>
2024-02-20 18:32:41 +0000

    Apply changes to release

--- dist/Manga_OnlineViewer.user.js
+++ dist/Manga_OnlineViewer.user.js
@@ -6,7 +6,7 @@
 // @supportURL    https://github.com/TagoDR/MangaOnlineViewer/issues
 // @namespace     https://github.com/TagoDR
 // @description   Shows all pages at once in online view for these sites: Alandal, Batoto, BilibiliComics, ComiCastle, Comick, Dynasty-Scans, MangaStream WordPress Plugin, Asura Scans, Flame Comics, Rizzcomic, Voids-Scans, Luminous Scans, Shimada Scans, Night Scans, Manhwa-Freak, OzulScansEn, AzureManga, CypherScans, MangaGalaxy, INKR, InManga, KLManga, Leitor, LHTranslation, Local Files, LynxScans, MangaBuddy, MangaDex, MangaFox, MangaHere, MangaFreak, Mangago, MangaHosted, MangaHub, MangasIn, MangaKakalot, MangaNelo, MangaNato, MangaPark, Mangareader, MangaSee, Manga4life, MangaTigre, MangaToons, MangaTown, ManhuaScan, MangaGeko, NaniScans, NineManga, OlympusScans, PandaManga, RawDevart, ReadComicsOnline, ReadManga Today, Funmanga, MangaDoom, MangaInn, ReaperScans, SenManga(Raw), KLManga, TenManga, TuMangaOnline, TuManhwas, UnionMangas, WebNovel, WebToons, Manga33, YugenMangas, ZeroScans, FoOlSlide, Kireicake, Madara WordPress Plugin, MangaHaus, Isekai Scan, Comic Kiba, Zinmanga, mangatx, Toonily, Mngazuki, JaiminisBox, DisasterScans, ManhuaPlus, TopManhua, NovelMic, Reset-Scans, LeviatanScans, Dragon Tea, SetsuScans, ToonGod
-// @version       2024.02.18
+// @version       2024.02.20
 // @license       MIT
 // @icon          https://cdn-icons-png.flaticon.com/32/2281/2281832.png
 // @run-at        document-end
@@ -4109,7 +4109,7 @@
     updateSettings({ bookmarks: marks });
     reloadBookmarks();
     document
-      .querySelectorAll(".BookmarkItem .erase")
+      .querySelectorAll(".bookmarkFunctions .erase")
       ?.forEach(addEvent("click", buttonEraseBookmarks));
   }
   function buttonBookmark(event) {
@@ -4155,7 +4155,7 @@
     }
     reloadBookmarks();
     document
-      .querySelectorAll(".BookmarkItem .erase")
+      .querySelectorAll(".bookmarkFunctions .erase")
       ?.forEach(addEvent("click", buttonEraseBookmarks));
   }
   function bookmarks() {
@@ -4169,7 +4169,7 @@
       .querySelector("#Overlay")
       ?.addEventListener("click", buttonBookmarksClose);
     document
-      .querySelectorAll(".BookmarkItem .erase")
+      .querySelectorAll(".bookmarkFunctions .erase")
       ?.forEach(addEvent("click", buttonEraseBookmarks));
     document
       .querySelectorAll(".Bookmark")
--- dist/Manga_OnlineViewer_Adult.user.js
+++ dist/Manga_OnlineViewer_Adult.user.js
@@ -6,7 +6,7 @@
 // @supportURL    https://github.com/TagoDR/MangaOnlineViewer/issues
 // @namespace     https://github.com/TagoDR
 // @description   Shows all pages at once in online view for these sites: BestPornComix, DoujinMoeNM, 8Muses.com, 8Muses.io, ExHentai, e-Hentai, FSIComics, GNTAI.net, HBrowser, Hentai2Read, HentaiFox, HentaiHand, nHentai.com, HentaIHere, HentaiNexus, hitomi, Imhentai, KingComix, Luscious, MultPorn, MyHentaiGallery, nHentai.net, nHentai.xxx, lhentai, 9Hentai, OmegaScans, PornComixOnline, Pururin, Simply-Hentai, Anchira, TMOHentai, 3Hentai, Tsumino, vermangasporno, vercomicsporno, wnacg, XlecxOne, xyzcomics, Madara WordPress Plugin, AllPornComic
-// @version       2024.02.18
+// @version       2024.02.20
 // @license       MIT
 // @icon          https://cdn-icons-png.flaticon.com/32/9824/9824312.png
 // @run-at        document-end
@@ -3617,7 +3617,7 @@
     updateSettings({ bookmarks: marks });
     reloadBookmarks();
     document
-      .querySelectorAll(".BookmarkItem .erase")
+      .querySelectorAll(".bookmarkFunctions .erase")
       ?.forEach(addEvent("click", buttonEraseBookmarks));
   }
   function buttonBookmark(event) {
@@ -3663,7 +3663,7 @@
     }
     reloadBookmarks();
     document
-      .querySelectorAll(".BookmarkItem .erase")
+      .querySelectorAll(".bookmarkFunctions .erase")
       ?.forEach(addEvent("click", buttonEraseBookmarks));
   }
   function bookmarks() {
@@ -3677,7 +3677,7 @@
       .querySelector("#Overlay")
       ?.addEventListener("click", buttonBookmarksClose);
     document
-      .querySelectorAll(".BookmarkItem .erase")
+      .querySelectorAll(".bookmarkFunctions .erase")
       ?.forEach(addEvent("click", buttonEraseBookmarks));
     document
       .querySelectorAll(".Bookmark")
