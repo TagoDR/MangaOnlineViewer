aa532a0578b84eddf9eaab768bf91eb2a4c5d669
TagoDR <TagoDR@github.com>
2024-03-19 17:44:36 -0300

    Fixed bilibilicomics.com to .net

--- src/main/bilibilicomics.ts
+++ src/main/bilibilicomics.ts
@@ -1,58 +1,23 @@
 // == BilibiliComics ===============================================================================
 export default {
   name: 'BilibiliComics',
-  url: /https?:\/\/(www\.)?(bilibilicomics).com\/.+\/.+/,
-  homepage: 'https://www.bilibilicomics.com/',
+  url: /https?:\/\/(www\.)?(bilibilicomics).net\/episode\/.+/,
+  homepage: 'https://www.bilibilicomics.net/',
   language: ['English'],
   category: 'manga',
-  waitEle: '.read-nav',
+  waitEle: '#__NUXT_DATA__',
   async run() {
-    const api = await fetch(
-      'https://www.bilibilicomics.com/twirp/comic.v1.Comic/GetImageIndex?device=pc&platform=web&lang=en&sys_lang=en',
-      {
-        method: 'post',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          ep_id: window.location.href.split('/').pop(),
-          credential: '',
-        }),
-      },
-    )
-      .then(async (res) => res.json())
-      .then(({ data }) => data.images.map((image: { path: string }) => `${image.path}@2000w.webp`))
-      .then(JSON.stringify)
-      .then(async (urls) =>
-        fetch(
-          'https://www.bilibilicomics.com/twirp/comic.v1.Comic/ImageToken?device=pc&platform=web&lang=en&sys_lang=en',
-          {
-            method: 'post',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ urls }),
-          },
-        ),
-      )
-      .then(async (res) => res.json())
-      .then((tokens) =>
-        tokens.data.map((i: { url: string; token: string }) => `${i.url}?token=${i.token}`),
-      );
-
+    const json: [any] = JSON.parse(document.querySelector('#__NUXT_DATA__')?.innerHTML ?? '');
+    const images = json.filter(
+      (x) => typeof x === 'string' && /.(png|jpg|jpeg|gif|bmp|webp)$/i.exec(x),
+    );
     return {
-      title: document.querySelector('.read-nav')?.textContent?.trim(),
-      series: document.querySelector('.manga-title')?.getAttribute('href'),
-      pages: api.length,
-      prev: document
-        .querySelector('.navigate a button[title=Previous]')
-        ?.parentElement?.getAttribute('href'),
-      next: document
-        .querySelector('.navigate a button[title=Next]')
-        ?.parentElement?.getAttribute('href'),
-      listImages: api,
+      title: document.querySelector('.chapterTitle')?.textContent?.trim(),
+      series: document.querySelector('.book-name')?.getAttribute('href'),
+      pages: images.length,
+      prev: document.querySelectorAll('.pre-next-btns').item(0)?.getAttribute('href'),
+      next: document.querySelectorAll('.pre-next-btns').item(2)?.getAttribute('href'),
+      listImages: images.map((image) => `https://static.comicfans.io/${image}`),
     };
   },
 };
