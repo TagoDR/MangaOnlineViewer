2a7e44f9b421647cc039820c1caa3c42db2e703f
TagoDR <TagoDR@github.com>
2024-04-21 10:50:49 -0300

    Fix 9hentai

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import display from './ui';
 import { allowUpload } from './core/upload';
 
 allowUpload();
-document.querySelector('#test')?.addEventListener('click', () => display(localhost.run()));
-const timeout = setTimeout(() => {
-  display(localhost.run());
-}, 2000);
-document.addEventListener('mousemove', () => clearTimeout(timeout), { once: true });
+// document.querySelector('#test')?.addEventListener('click', () => display(localhost.run()));
+// const timeout = setTimeout(() => {
+display(localhost.run());
+// }, 2000);
+// document.addEventListener('mousemove', () => clearTimeout(timeout), { once: true });
