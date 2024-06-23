ee77206644d70561287c9340f0aeb20aa358c335
TagoDR <TagoDR@github.com>
2024-06-23 11:44:26 -0300

    Fix Fakku.cc

--- src/adult/fakku.ts
+++ src/adult/fakku.ts
@@ -1,22 +1,32 @@
 // == Fakku.cc =====================================================================================
+
 export default {
   name: 'Fakku.cc',
-  url: /https?:\/\/spy.fakku.cc\/archive\/\d+\/.+\/\d+/,
+  url: /https?:\/\/(spy.)?fakku.cc/,
   homepage: 'https://spy.fakku.cc/',
   language: ['English'],
   category: 'hentai',
-  run() {
+  waitEle: 'div div + img[alt^=Page]',
+  async run() {
     const cdn = 'https://cdn.fakku.cc';
-    const num = parseInt(document.body.dataset.totalPages ?? '0', 10);
+    const api = await fetch(`${window.location.href}/__data.json?x-sveltekit-invalidated=011`).then(
+      async (res) => res.text(),
+    );
+    const { data } = JSON.parse(api.split('\n').filter((s) => s.length)[1]);
+    const slug = data[5];
+    const num = data[12].length;
     return {
-      title: document.querySelector('.title')?.textContent?.trim(),
-      series: document.querySelector('a.back')?.getAttribute('href'),
+      title: document
+        .querySelector('.title')
+        ?.textContent?.trim()
+        .replace(/Page \d+ • /, ''),
+      series: window.location.href.replace(/read\/.+/, ''),
       pages: num,
       prev: '#',
       next: '#',
       listImages: Array(num)
         .fill(0)
-        .map((_, i) => `${cdn}/data/${document.body.dataset.id}/${i + 1}.jpg`),
+        .map((_, i) => `${cdn}/image/${slug}/${i + 1}`),
     };
   },
 };
