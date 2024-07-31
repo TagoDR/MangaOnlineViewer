1547de4fc429995289de838d596c614105e98f4d
TagoDR <TagoDR@github.com>
2024-07-31 18:49:32 -0300

    Fix Reaperscans

--- src/main/reaperscans.ts
+++ src/main/reaperscans.ts
@@ -1,20 +1,19 @@
 // == ReaperScans ==================================================================================
 export default {
   name: 'ReaperScans',
-  url: /https?:\/\/(www\.)?reaperscans\.com\/comics\/.+\/chapters\/.+/,
+  url: /https?:\/\/(www\.)?reaperscans\.com\/series\/.+\/chapter.+/,
   homepage: 'https://reaperscans.com/',
   language: ['English'],
   category: 'manga',
-  waitEle: 'main img',
   run() {
-    const images = [...document.querySelectorAll('main img')];
+    const images = [...document.querySelectorAll('#content .container img:not(.rounded)')];
     return {
       title: document.querySelector('title')?.textContent?.trim(),
-      series: document.querySelector('.fa-list')?.parentElement?.getAttribute('href'),
+      series: document.querySelector('button .fa-house')?.closest('a')?.getAttribute('href'),
       pages: images.length,
-      prev: document.querySelector('.fa-arrow-left-long')?.parentElement?.getAttribute('href'),
-      next: document.querySelector('.fa-arrow-right-long')?.parentElement?.getAttribute('href'),
-      listImages: images.map((img) => img.getAttribute('data-src') ?? img.getAttribute('src')),
+      prev: document.querySelector('.fa-chevron-left')?.closest('a')?.getAttribute('href'),
+      next: document.querySelector('.fa-chevron-right')?.closest('a')?.getAttribute('href'),
+      listImages: images.map((img) => img.getAttribute('data-src') || img.getAttribute('src')),
     };
   },
 };
