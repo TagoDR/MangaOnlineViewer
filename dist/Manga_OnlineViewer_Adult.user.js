b2eb9779823f852016a8ad104b294d9a15cdb8cf
TagoDR <TagoDR@users.noreply.github.com>
2024-12-27 21:22:25 +0000

    Apply changes to release

--- dist/Manga_OnlineViewer_Adult.user.js
+++ dist/Manga_OnlineViewer_Adult.user.js
@@ -6,7 +6,7 @@
 // @supportURL    https://github.com/TagoDR/MangaOnlineViewer/issues
 // @namespace     https://github.com/TagoDR
 // @description   Shows all pages at once in online view for these sites: AkumaMoe, BestPornComix, DoujinMoeNM, 8Muses.com, 8Muses.io, ExHentai, e-Hentai, FSIComics, GNTAI.net, HBrowser, Hentai2Read, HentaiEra, HentaiFox, HentaiHand, nHentai.com, HentaIHere, HentaiNexus, HenTalk, hitomi, Imhentai, KingComix, Chochox, Comics18, Luscious, ManhwaRead, MultPorn, MyHentaiGallery, nHentai.net, nHentai.xxx, lhentai, 9Hentai, OmegaScans, PornComixOnline, Pururin, SchaleNetwork, Simply-Hentai, TMOHentai, 3Hentai, HentaiVox, Tsumino, vermangasporno, vercomicsporno, wnacg, XlecxOne, xyzcomics, Madara WordPress Plugin, AllPornComic, Manytoon, Manga District
-// @version       2024.12.26
+// @version       2024.12.27
 // @license       MIT
 // @icon          https://cdn-icons-png.flaticon.com/32/9824/9824312.png
 // @run-at        document-end
@@ -1324,15 +1324,20 @@
   const isMobile = () => getDevice() === "mobile" || getDevice() === "tablet";
   const settingsChangeListener = (fn) => {
     if (typeof GM_addValueChangeListener !== "undefined") {
-      return GM_addValueChangeListener(
-        "settings",
-        (_name, _oldValue, newValue, remote) => {
-          if (remote) fn(newValue);
-        },
-      );
-    } else {
-      return false;
+      try {
+        return GM_addValueChangeListener(
+          "settings",
+          (_name, _oldValue, newValue, remote) => {
+            if (remote) fn(newValue);
+          },
+        );
+      } finally {
+      }
     }
+    logScript("Using Interval Settings Change Listener");
+    return setInterval(() => {
+      fn(getSettings());
+    }, 1e4);
   };
 
   async function fetchText(url, format) {
