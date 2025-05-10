bbed31ea1744ae36969ca19b7aed5ce8c61a68ee
TagoDR <tagodr@users.noreply.github.com>
2025-05-10 20:31:22 -0300

    Fixed HenTalk

--- src/adult/hentalk.ts
+++ src/adult/hentalk.ts
@@ -6,32 +6,27 @@ export default {
   homepage: 'https://hentalk.pw/',
   language: ['English'],
   category: 'hentai',
-  waitEle: 'div div + img[alt^=Page]',
   async run() {
     const cdn = 'https://hentalk.pw';
     const api = await fetch(
-      `${window.location.pathname}/__data.json?x-sveltekit-invalidated=011`,
-    ).then(async (res) => res.json());
-    const slug = document
-      .querySelector('div div + img[alt^=Page]')
-      ?.getAttribute('src')
-      ?.match(/image\/(.+)\//)?.[1];
-    const images = api.nodes[1].data.filter(
-      (e: unknown) => typeof e === 'string' && /\.(png|gif|jpg|jpeg|webp)$/.test(e),
-    );
+      `${window.location.pathname}/__data.json?x-sveltekit-trailing-slash=1&x-sveltekit-invalidated=001`,
+    )
+      .then(async (res) => res.json())
+      .then((j) => j.nodes[2].data);
+    const gallery = api?.[api.find((e: { gallery?: number }) => e?.gallery)?.gallery];
+    const slug =
+      api?.[gallery?.hash] ||
+      api?.[api.find((e: { hash?: number; id?: number }) => e?.hash && e?.id).hash];
+    const images = api?.[gallery.images]
+      .map((i: number) => api[i])
+      .map((i: { filename: string }) => api[i.filename]);
     return {
-      title: document
-        .querySelector('title')
-        ?.textContent?.trim()
-        .replace(/Page \d+ • /, ''),
+      title: api?.[gallery.title],
       series: window.location.href.replace(/read\/.+/, ''),
       pages: images?.length,
       prev: '#',
       next: '#',
-      listImages: images?.map(
-        (src: string, i: number) =>
-          `${cdn}/image/${slug}/${i + 1}${/\.(png|gif|jpg|jpeg|webp)$/.exec(src)?.[0]}`,
-      ),
+      listImages: images?.map((src: string) => `${cdn}/image/${slug}/${src}`),
     };
   },
 };
