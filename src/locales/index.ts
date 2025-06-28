844d559d9251ffb73d1b87ec5ae39bd75ad8ce1b
Big cat loaf <100969549+big-cat-loaf@users.noreply.github.com>
2025-06-28 15:10:32 +0200

    Update index.ts

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -3,7 +3,8 @@ import en_US from './en_US';
 import pt_BR from './pt_BR';
 import zh_CN from './zh_CN';
 import es_ES from './es_ES';
+import de_DE from './de_DE';
 
-const locales = [en_US, es_ES, pt_BR, zh_CN] as ILocale[];
+const locales = [en_US, es_ES, pt_BR, zh_CN, de_DE] as ILocale[];
 
 export default locales;
