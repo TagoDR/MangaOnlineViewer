99c80ecb8ce7f55182e096c66e031ec228db936d
TagoDR <tagodr@users.noreply.github.com>
2025-08-12 14:56:06 -0300

    Refactor navigation bar styles and logic; add hover transitions, customizable header adjustments, and improved orientation handling for better flexibility and usability.

--- src/ui/components/ThumbnailsPanel.ts
+++ src/ui/components/ThumbnailsPanel.ts
@@ -1,4 +1,4 @@
-import { html } from 'lit';
+import { html, nothing } from 'lit';
 import { classMap } from 'lit/directives/class-map.js';
 import { getAppStateValue, getLocaleString, getSettingsValue } from '../../core/settings';
 import type { IManga } from '../../types';
@@ -7,48 +7,55 @@ import { transformScrollToHorizontal } from '../events/common';
 import { clickThumbnail } from '../events/navigation';
 import { IconCategory } from '../icons';
 
-const ThumbnailsPanel = (manga: IManga) => html`
-  <nav
-    id="Navigation"
-    class="${classMap({
-      [getSettingsValue('navbar')]: true,
-      panel: true,
-    })}"
-  >
-    <div
-      id="NavigationCounters"
-      class="ControlLabel"
+const ThumbnailsPanel = (manga: IManga) => {
+  const navbarPosition = getSettingsValue('navbar');
+  const isVertical = navbarPosition === 'left' || navbarPosition === 'right';
+  return html`
+    <nav
+      id="Navigation"
+      class="${classMap({
+        [navbarPosition]: true,
+        panel: true,
+        vertical: isVertical,
+        horizontal: !isVertical,
+        header: getAppStateValue('headerVisible'),
+      })}"
     >
-      ${IconCategory}
-      <i>${getAppStateValue('loaded')}</i> /
-      <b>
-        ${(getAppStateValue('manga')?.pages ?? 0) - ((getAppStateValue('manga')?.begin ?? 1) - 1)}
-      </b>
-      ${getLocaleString('PAGES_LOADED')}
-      <span>: ${getAppStateValue('currentPage')}</span>
-    </div>
-    <div
-      id="Thumbnails"
-      @wheel=${transformScrollToHorizontal}
-    >
-      ${sequence(manga.pages, manga.begin).map(
-        index => html`
-          <div
-            id="Thumbnail${index}"
-            class="Thumbnail"
-          >
-            <img
-              id="ThumbnailImg${index}"
-              alt=""
-              class="ThumbnailImg"
-              src=${getAppStateValue('images')?.[index]?.src ?? ''}
-              @click=${clickThumbnail}
-            />
-            <span class="ThumbnailIndex">${index}</span>
-          </div>
-        `,
-      )}
-    </div>
-  </nav>
-`;
+      <div
+        id="NavigationCounters"
+        class="ControlLabel"
+      >
+        ${IconCategory}
+        <i>${getAppStateValue('loaded')}</i> /
+        <b>
+          ${(getAppStateValue('manga')?.pages ?? 0) - ((getAppStateValue('manga')?.begin ?? 1) - 1)}
+        </b>
+        ${getLocaleString('PAGES_LOADED')}
+        <span>: ${getAppStateValue('currentPage')}</span>
+      </div>
+      <div
+        id="Thumbnails"
+        @wheel=${!isVertical ? transformScrollToHorizontal : nothing}
+      >
+        ${sequence(manga.pages, manga.begin).map(
+          index => html`
+            <div
+              id="Thumbnail${index}"
+              class="Thumbnail"
+            >
+              <img
+                id="ThumbnailImg${index}"
+                alt=""
+                class="ThumbnailImg"
+                src=${getAppStateValue('images')?.[index]?.src ?? ''}
+                @click=${clickThumbnail}
+              />
+              <span class="ThumbnailIndex">${index}</span>
+            </div>
+          `,
+        )}
+      </div>
+    </nav>
+  `;
+};
 export default ThumbnailsPanel;
