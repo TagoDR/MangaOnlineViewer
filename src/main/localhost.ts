db21eefa8192ed531d894430e1205f0770bf890f
TagoDR <tagodr@users.noreply.github.com>
2025-09-20 09:23:07 -0300

    Better Comment for testing

--- src/main/localhost.ts
+++ src/main/localhost.ts
@@ -11,7 +11,19 @@ const localhost: ISite = {
   run(): IManga {
     const num: number = parseInt(/\d+/.exec(window.location.search)?.toString() ?? '5', 10);
     const comments = document.createElement('div');
-    comments.innerHTML = Array(100).fill('Testing Comment<br/>').join('');
+    const lorem =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    const commentsEn = [
+      lorem,
+      `<span style="color: white; background-color: black;">${lorem.substring(0, 100)}</span>`,
+      `<span style="color: black; background-color: white;">${lorem.substring(100, 200)}</span>`,
+      `<b>${lorem.substring(200, 300)}</b>`,
+      `<i>${lorem.substring(300, 400)}</i>`,
+    ];
+    comments.innerHTML = Array(100)
+      .fill(0)
+      .map(() => commentsEn[Math.floor(Math.random() * commentsEn.length)])
+      .join('<br><br>');
     return {
       title: 'Placeholder Manga Loaded',
       series: '?reload',
