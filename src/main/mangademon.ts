c883b9ac4d72d79bac6a33c1a4c894ee41d2af59
TagoDR <tagodr@users.noreply.github.com>
2025-10-10 18:56:57 -0300

    Fix MangaDemon

--- src/main/mangademon.ts
+++ src/main/mangademon.ts
@@ -7,26 +7,20 @@ const mangademon: ISite = {
   homepage: 'https://demonicscans.org/',
   language: [Language.ENGLISH],
   category: Category.MANGA,
-  run(): IManga {
-    const images = [...document.querySelectorAll('.imgholder')];
+  async run(): Promise<IManga> {
+    const response = await fetch(location.href);
+    const text = await response.text();
+    const doc = new DOMParser().parseFromString(text, 'text/html');
+    const images = [...doc.querySelectorAll('.imgholder')];
     return {
-      title: document.querySelector('title')?.textContent?.trim(),
-      series: document.querySelector('h1 a')?.getAttribute('href'),
+      title: doc.querySelector('title')?.textContent?.trim(),
+      series: doc.querySelector('h1 a')?.getAttribute('href'),
       pages: images.length,
-      prev: document.querySelector('.prevchap')?.getAttribute('href'),
-      next: document.querySelector('.nextchap')?.getAttribute('href'),
+      prev: doc.querySelector('.prevchap')?.getAttribute('href'),
+      next: doc.querySelector('.nextchap')?.getAttribute('href'),
       listImages: images.map(
-        img => (img.getAttribute('data-src') || img.getAttribute('src')) ?? '',
+        (img) => (img.getAttribute('data-src') || img.getAttribute('src')) ?? '',
       ),
-      before(): void {
-        // Restore Tampered document.createTextNode
-        const iframe = document.createElement('iframe');
-        iframe.style.display = 'none';
-        document.body.appendChild(iframe);
-        const originalDocument = iframe.contentWindow?.document;
-        if (originalDocument) document.createTextNode = originalDocument.createTextNode;
-        document.body.removeChild(iframe);
-      },
     };
   },
 };
