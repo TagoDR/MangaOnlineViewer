f6463eaf864d2091e27fcf5bce6729d921a7c1d6
TagoDR <tagodr@users.noreply.github.com>
2025-12-24 22:05:37 -0300

    Added Series keybind for num_decimal

--- src/core/settings.ts
+++ src/core/settings.ts
@@ -62,7 +62,7 @@ export const defaultSettings: ISettings = {
     SCROLL_DOWN: ['down', 'S', 'num_2'],
     NEXT_CHAPTER: ['right', '/', 'D', 'num_6'],
     PREVIOUS_CHAPTER: ['left', ';', 'A', 'num_4'],
-    RETURN_CHAPTER_LIST: ['backspace', 'del'],
+    RETURN_CHAPTER_LIST: ['backspace', 'del', 'num_decimal'],
     ENLARGE: ['-', 'num_add', 'E'],
     REDUCE: ['=', 'num_subtract', 'Q'],
     RESTORE: ['9', 'num_divide', 'R'],
