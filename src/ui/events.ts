7e371e05a5adc9a47481eea5618915f1d1e35640
TagoDR <tagodr@users.noreply.github.com>
2026-01-22 21:32:03 -0300

    Refresh Zoom on resize and orientantion change

--- src/ui/events.ts
+++ src/ui/events.ts
@@ -1,9 +1,20 @@
+import _ from 'lodash';
 import { getAppStateValue, setAppStateValue } from '../core/settings.ts';
-import { getDevice } from '../utils/tampermonkey';
+import { getDevice } from '../utils/tampermonkey.ts';
 import { waitForFunc } from '../utils/waitFor.ts';
 import autoscroll from './events/autoscroll';
 import trackCurrentPage from './events/currentpage';
 import keybindings from './events/keybindings';
+import { applyZoom } from './events/zoom.ts';
+
+/**
+ * Event handler for window resize and orientation change events.
+ * It recalculates the zoom to ensure the content fits the new window dimensions.
+ */
+export const updateOnResize = _.debounce(() => {
+  setAppStateValue('device', getDevice());
+  applyZoom();
+}, 200);
 
 /**
  * Initializes all global event listeners for the application.
@@ -13,9 +24,8 @@ import keybindings from './events/keybindings';
 async function events() {
   await waitForFunc(() => getAppStateValue('manga') !== undefined);
   keybindings();
-  window.addEventListener('resize', () => {
-    setAppStateValue('device', getDevice());
-  });
+  window.addEventListener('resize', updateOnResize);
+  window.addEventListener('orientationchange', updateOnResize);
   autoscroll();
   trackCurrentPage();
 }
