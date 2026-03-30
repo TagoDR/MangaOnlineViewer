704068dddc93d06df5916068f46bc3a904cbded3
TagoDR <tagodr@users.noreply.github.com>
2026-03-29 22:16:09 -0300

    Fix nhentai

--- src/adult/nhentainet.ts
+++ src/adult/nhentainet.ts
@@ -1,32 +1,27 @@
 // == nHentai.net ==================================================================================
 import { Category, type IManga, type ISite, Language } from '../types';
-import { extensionByCode } from '../utils/urls';
 
 const nhentainet: ISite = {
-  name: ['nHentai.net', 'nHentai.xxx', 'lhentai'],
-  url: /https?:\/\/(www\.)?(nhentai|lhentai).(net|xxx|com|to)\/g\/.+\/.+/,
-  homepage: ['https://nhentai.net/', 'https://nhentai.xxx/', 'https://lhentai.com/'],
+  name: ['nHentai.net'],
+  url: /https?:\/\/(www\.)?(nhentai).(net|xxx|com|to)\/g\/.+\/.+/,
+  homepage: ['https://nhentai.net/'],
   language: [Language.ENGLISH],
   category: Category.HENTAI,
-  run(): IManga {
-    const num = parseInt(document.querySelector('.num-pages')?.textContent ?? '', 10);
-    const src = document
-      .querySelector('#image-container img')
-      ?.getAttribute('src')
-      ?.replace(/\d+.\w+$/, '');
-
-    const ext = unsafeWindow._gallery?.images?.pages?.map((i: { t: string }) =>
-      extensionByCode(i.t),
-    );
+  async run(): Promise<IManga> {
+    const cdn = await fetch('https://nhentai.net/api/v2/config').then(async res => res.json());
+    const api = await fetch(
+      `https://nhentai.net/api/v2/galleries/${window.location.pathname.split('/')[2]}`,
+    ).then(async res => res.json());
     return {
       title: document.querySelector('title')?.textContent?.split('- Page')[0].trim(),
       series: document.querySelector('.go-back')?.getAttribute('href'),
-      pages: num,
+      pages: api.pages.length,
       prev: '#',
       next: '#',
-      listImages: Array(num)
-        .fill(0)
-        .map((_, i) => `${src}${i + 1}.${ext[i]}`),
+      listImages: api.pages.map(
+        (img: { path: string }) =>
+          `${cdn.image_servers[Math.floor(Math.random() * cdn.image_servers.length)]}/${img.path}`,
+      ),
     };
   },
 };
