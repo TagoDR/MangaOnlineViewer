228233dd16d7982729d4ea772821469d2081c028
TagoDR <tagodr@users.noreply.github.com>
2026-04-19 21:53:33 -0300

    Lit style for CSS

--- src/ui/styles/index.ts
+++ src/ui/styles/index.ts
@@ -3,7 +3,8 @@
  * It imports individual CSS files as inline strings, defines base theme variables for light and dark modes,
  * and combines them into a single CSS template literal that can be injected into the application's shadow DOM.
  */
-import { css } from '../../utils/code-tag';
+
+import { css, unsafeCSS } from 'lit';
 import { svgToUrl } from '../../utils/svgs';
 import { IconPhoto, IconPhotoOff } from '../icons/StyledIcons.ts';
 import styles from './App.css?inline';
@@ -14,23 +15,25 @@ import gallery from './gallery.css?inline';
 import media from './media.css?inline';
 import page from './page.css?inline';
 import normalize from './simplenormalize.css?inline';
+import theme from './theme.css?inline';
 
 export default css`
   .PageContent .PageImg[src=''],
   .PageContent .PageImg:not([src]) {
-    background-image: url('${svgToUrl(IconPhoto)}');
+    background-image: url('${unsafeCSS(svgToUrl(IconPhoto))}');
   }
 
   .PageContent .PageImg.imgBroken {
-    background-image: url('${svgToUrl(IconPhotoOff)}');
+    background-image: url('${unsafeCSS(svgToUrl(IconPhotoOff))}');
   }
 
-  ${normalize}
-  ${styles}
-  ${page}
-  ${fluid}
-  ${book}
-  ${gallery}
-  ${media}
-  ${animation}
+  ${unsafeCSS(theme)}
+  ${unsafeCSS(normalize)}
+  ${unsafeCSS(styles)}
+  ${unsafeCSS(page)}
+  ${unsafeCSS(fluid)}
+  ${unsafeCSS(book)}
+  ${unsafeCSS(gallery)}
+  ${unsafeCSS(media)}
+  ${unsafeCSS(animation)}
 `;
--- src/ui/themes.ts
+++ src/ui/themes.ts
@@ -1,5 +1,5 @@
+import { css, unsafeCSS } from 'lit';
 import { getSettingsValue } from '../core/settings';
-import { css } from '../utils/code-tag';
 import colors, { getTextColor } from '../utils/colors';
 import { generateColorGradient } from '../utils/palettes.ts';
 
@@ -17,17 +17,17 @@ const themesCSS = (selector: string = '#MangaOnlineViewer', hex = getSettingsVal
   const secondary = getSettingsValue('colorScheme') === 'dark' ? gradient[8] : gradient[2];
   const secondaryText = getTextColor(secondary);
 
-  //   --mov-color-custom-95: ${gradient[0]};
-  //   --mov-color-custom-90: ${gradient[1]};
-  //   --mov-color-custom-80: ${gradient[2]};
-  //   --mov-color-custom-70: ${gradient[3]};
-  //   --mov-color-custom-60: ${gradient[4]};
-  //   --mov-color-custom-50: ${gradient[5]};
-  //   --mov-color-custom-40: ${gradient[6]};
-  //   --mov-color-custom-30: ${gradient[7]};
-  //   --mov-color-custom-20: ${gradient[8]};
-  //   --mov-color-custom-10: ${gradient[9]};
-  //   --mov-color-custom-05: ${gradient[10]};
+  //   --mov-color-custom-95: ${unsafeCSS(gradient[0])};
+  //   --mov-color-custom-90: ${unsafeCSS(gradient[1])};
+  //   --mov-color-custom-80: ${unsafeCSS(gradient[2])};
+  //   --mov-color-custom-70: ${unsafeCSS(gradient[3])};
+  //   --mov-color-custom-60: ${unsafeCSS(gradient[4])};
+  //   --mov-color-custom-50: ${unsafeCSS(gradient[5])};
+  //   --mov-color-custom-40: ${unsafeCSS(gradient[6])};
+  //   --mov-color-custom-30: ${unsafeCSS(gradient[7])};
+  //   --mov-color-custom-20: ${unsafeCSS(gradient[8])};
+  //   --mov-color-custom-10: ${unsafeCSS(gradient[9])};
+  //   --mov-color-custom-05: ${unsafeCSS(gradient[10])};
   //   --mov-color-custom: var(--mov-color-custom-60);
   //   --mov-color-custom-key: 60;
   //   --mov-color-custom-gte-60: calc(
@@ -41,29 +41,29 @@ const themesCSS = (selector: string = '#MangaOnlineViewer', hex = getSettingsVal
 
   return css`
     :where(:root),
-    ${selector}, .dark,
-    ${selector}.dark {
-      --theme-primary-color: ${hex};
-      --theme-primary-text-color: ${text};
-      --theme-secondary-color: ${secondary};
-      --theme-secondary-text-color: ${secondaryText};
+    ${unsafeCSS(unsafeCSS(selector))}, .dark,
+    ${unsafeCSS(unsafeCSS(selector))}.dark {
+      --theme-primary-color: ${unsafeCSS(unsafeCSS(hex))};
+      --theme-primary-text-color: ${unsafeCSS(unsafeCSS(text))};
+      --theme-secondary-color: ${unsafeCSS(unsafeCSS(secondary))};
+      --theme-secondary-text-color: ${unsafeCSS(unsafeCSS(secondaryText))};
 
       color-scheme: dark;
-      --theme-body-background: ${colors.dark['600']};
-      --theme-body-text-color: ${colors.dark['50']};
-      --theme-text-color: ${colors.dark['50']};
-      --theme-background-color: ${colors.dark['600']};
-      --theme-hightlight-color: ${colors.dark['500']};
-      --theme-border-color: ${colors.dark['400']};
+      --theme-body-background: ${unsafeCSS(unsafeCSS(colors.dark['600']))};
+      --theme-body-text-color: ${unsafeCSS(unsafeCSS(colors.dark['50']))};
+      --theme-text-color: ${unsafeCSS(unsafeCSS(colors.dark['50']))};
+      --theme-background-color: ${unsafeCSS(unsafeCSS(colors.dark['600']))};
+      --theme-hightlight-color: ${unsafeCSS(unsafeCSS(colors.dark['500']))};
+      --theme-border-color: ${unsafeCSS(unsafeCSS(colors.dark['400']))};
 
-      --mov-color-fill-quiet: ${gradient[9]};
-      --mov-color-fill-normal: var(--theme-secondary-color, ${gradient[8]});
+      --mov-color-fill-quiet: ${unsafeCSS(unsafeCSS(gradient[9]))};
+      --mov-color-fill-normal: var(--theme-secondary-color, ${unsafeCSS(unsafeCSS(gradient[8]))});
       --mov-color-fill-loud: var(--theme-primary-color);
-      --mov-color-border-quiet: ${gradient[8]};
-      --mov-color-border-normal: ${gradient[7]};
-      --mov-color-border-loud: ${gradient[6]};
-      --mov-color-on-quiet: ${gradient[4]};
-      --mov-color-on-normal: var(--theme-secondary-text-color, ${gradient[3]});
+      --mov-color-border-quiet: ${unsafeCSS(unsafeCSS(gradient[8]))};
+      --mov-color-border-normal: ${unsafeCSS(unsafeCSS(gradient[7]))};
+      --mov-color-border-loud: ${unsafeCSS(unsafeCSS(gradient[6]))};
+      --mov-color-on-quiet: ${unsafeCSS(unsafeCSS(gradient[4]))};
+      --mov-color-on-normal: var(--theme-secondary-text-color, ${unsafeCSS(unsafeCSS(gradient[3]))});
       --mov-color-on-loud: var(--theme-primary-text-color, white);
 
       --mov-color-mix-hover: black 8%;
@@ -71,23 +71,23 @@ const themesCSS = (selector: string = '#MangaOnlineViewer', hex = getSettingsVal
     }
 
     .light,
-    ${selector}.light {
+    ${unsafeCSS(unsafeCSS(selector))}.light {
       color-scheme: light;
-      --theme-body-background: ${colors.gray['50']};
-      --theme-body-text-color: ${colors.gray['900']};
-      --theme-text-color: ${colors.gray['900']};
-      --theme-background-color: ${colors.gray['50']};
-      --theme-hightlight-color: ${colors.gray['500']};
-      --theme-border-color: ${colors.gray['100']};
+      --theme-body-background: ${unsafeCSS(unsafeCSS(colors.gray['50']))};
+      --theme-body-text-color: ${unsafeCSS(unsafeCSS(colors.gray['900']))};
+      --theme-text-color: ${unsafeCSS(unsafeCSS(colors.gray['900']))};
+      --theme-background-color: ${unsafeCSS(unsafeCSS(colors.gray['50']))};
+      --theme-hightlight-color: ${unsafeCSS(unsafeCSS(colors.gray['500']))};
+      --theme-border-color: ${unsafeCSS(unsafeCSS(colors.gray['100']))};
 
-      --mov-color-fill-quiet: ${gradient[0]};
-      --mov-color-fill-normal: var(--theme-secondary-color, ${gradient[1]});
+      --mov-color-fill-quiet: ${unsafeCSS(unsafeCSS(gradient[0]))};
+      --mov-color-fill-normal: var(--theme-secondary-color, ${unsafeCSS(unsafeCSS(gradient[1]))});
       --mov-color-fill-loud: var(--theme-primary-color);
-      --mov-color-border-quiet: ${gradient[1]};
-      --mov-color-border-normal: ${gradient[2]};
-      --mov-color-border-loud: ${gradient[4]};
-      --mov-color-on-quiet: ${gradient[6]};
-      --mov-color-on-normal: var(--theme-secondary-text-color, ${gradient[3]});
+      --mov-color-border-quiet: ${unsafeCSS(unsafeCSS(gradient[1]))};
+      --mov-color-border-normal: ${unsafeCSS(unsafeCSS(gradient[2]))};
+      --mov-color-border-loud: ${unsafeCSS(unsafeCSS(gradient[4]))};
+      --mov-color-on-quiet: ${unsafeCSS(unsafeCSS(gradient[6]))};
+      --mov-color-on-normal: var(--theme-secondary-text-color, ${unsafeCSS(unsafeCSS(gradient[3]))});
       --mov-color-on-loud: var(--theme-primary-text-color, white);
 
       --mov-color-mix-hover: black 10%;
