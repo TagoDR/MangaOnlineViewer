abdbaec796f916cbfff292d9b0bdddd6ef848a77
TagoDR <tagodr@users.noreply.github.com>
2026-05-30 11:51:04 -0300

    Fix tmofans

--- src/adult/tmohentai.ts
+++ src/adult/tmohentai.ts
@@ -1,16 +1,14 @@
 // == TMOHentai ====================================================================================
 
-import { runTMO } from '../main/tmofans';
-import { Category, type IManga, type ISite, Language } from '../types';
+import TMO from '../main/tmofans';
+import { Category, type ISite, Language } from '../types';
 
 const tmohhentai: ISite = {
+  ...TMO,
   name: 'TMOHentai',
   url: /https?:\/\/(www\.)?tmohentai\.(com|app)\/(reader|library|view_uploads)\/.+/,
   homepage: 'https://tmohentai.app/',
   language: [Language.SPANISH],
   category: Category.HENTAI,
-  run(): IManga {
-    return runTMO();
-  },
 };
 export default tmohhentai;
